b58ddc8d4ced6c6fb32f01f251784124aca38d48
mstone9 <mstone9@missouriwestern.edu>
2025-09-09 22:00:13 -0500

    Changed base for main.tsx
    Base is now '/MovieApp'

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {HashRouter} from 'react-router-dom'
 import './css/index.css'
 import App from './App.tsx'
+import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <HashRouter>
-          <App />
-      </HashRouter>
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter basename="/MovieApp">
+            <App />
+        </BrowserRouter>
+    </StrictMode>,
 )
