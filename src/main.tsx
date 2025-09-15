1e63f4edb98cbb19fa43b96eb1ec917c277298ec
mstone9 <mstone9@missouriwestern.edu>
2025-09-15 10:47:21 -0500

    Updated main.tsx basename
    Base name is now "/MovieApp/"

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter basename="/MovieApp">
+        <BrowserRouter basename="/MovieApp/">
             <App />
         </BrowserRouter>
     </StrictMode>,
